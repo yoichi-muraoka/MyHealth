dc74d077c5f146a7cace989245bfbc7fb5be09ef
muraoka <ym.animator@gmail.com>
2022-07-12 16:07:24 +0900

    ユーザー追加メソッドの実装

--- src/main/java/dao/UserDaoImpl.java
+++ src/main/java/dao/UserDaoImpl.java
@@ -21,53 +21,62 @@ public class UserDaoImpl implements UserDao {
 	@Override
 	public User findByEmail(String email) throws Exception {
 		User user = null;
-	    try (Connection con = ds.getConnection()) {
-	      String sql = "SELECT * FROM users WHERE email = ?";
-	      PreparedStatement stmt = con.prepareStatement(sql);
-	      stmt.setString(1, email);
-	      ResultSet rs = stmt.executeQuery();
-	      if (rs.next()) {
-	        user = mapToUser(rs);
-	      }
-	    } catch (Exception e) {
-	      throw e;
-	    }
-	    return user;
+		try (Connection con = ds.getConnection()) {
+			String sql = "SELECT * FROM users WHERE email = ?";
+			PreparedStatement stmt = con.prepareStatement(sql);
+			stmt.setString(1, email);
+			ResultSet rs = stmt.executeQuery();
+			if (rs.next()) {
+				user = mapToUser(rs);
+			}
+		} catch (Exception e) {
+			throw e;
+		}
+		return user;
 	}
 
 	@Override
 	public User findByEmailAndPass(String email, String pass) throws Exception {
 		User user = findByEmail(email);
-		if(user == null) {
+		if (user == null) {
 			System.out.println("Emailアドレスが登録されていない");
 			return null;
 		}
-		
-		if(!BCrypt.checkpw(pass, user.getPass())) {
+
+		if (!BCrypt.checkpw(pass, user.getPass())) {
 			System.out.println("パスワードが間違っている");
 			return null;
 		}
-		
+
 		return user;
 	}
 
 	@Override
 	public void insert(User user) throws Exception {
-		// TODO 自動生成されたメソッド・スタブ
-		
+		try (Connection con = ds.getConnection()) {
+			String sql = "INSERT INTO users (name, height, email, pass)";
+			PreparedStatement stmt = con.prepareStatement(sql);
+			stmt.setString(1, user.getName());
+			stmt.setDouble(2, user.getHeight());
+			stmt.setString(3, user.getEmail());
+			stmt.setString(4, BCrypt.hashpw(user.getPass(), BCrypt.gensalt()));			
+			stmt.executeUpdate();
+		} catch (Exception e) {
+			throw e;
+		}
 	}
-	
+
 	@Override
 	public void updateHeight(int id, double height) throws Exception {
-	    try (Connection con = ds.getConnection()) {
-	      String sql = "UPDATE users SET height = ? WHERE id = ?";
-	      PreparedStatement stmt = con.prepareStatement(sql);
-	      stmt.setDouble(1, height);
-	      stmt.setInt(2, id);
-	      stmt.executeUpdate();
-	    } catch (Exception e) {
-	      throw e;
-	    }
+		try (Connection con = ds.getConnection()) {
+			String sql = "UPDATE users SET height = ? WHERE id = ?";
+			PreparedStatement stmt = con.prepareStatement(sql);
+			stmt.setDouble(1, height);
+			stmt.setInt(2, id);
+			stmt.executeUpdate();
+		} catch (Exception e) {
+			throw e;
+		}
 	}
 
 	protected User mapToUser(ResultSet rs) throws Exception {
