e2c1f74eb14a1c7b01ff3ad00f1a76857b2ae124
muraoka <ym.animator@gmail.com>
2022-07-12 11:23:09 +0900

    最新の記録を取得するメソッドを実装

---
+++ src/main/java/dao/DailyRecordDaoImpl.java
@@ -0,0 +1,78 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import domain.DailyRecord;
+
+public class DailyRecordDaoImpl implements DailyRecordDao {
+
+	private DataSource ds;
+
+	public DailyRecordDaoImpl(DataSource ds) {
+		this.ds = ds;
+	}
+
+	@Override
+	public List<DailyRecord> findNewerRecords(int userId) throws Exception {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+	@Override
+	public DailyRecord findLatestRecord(int userId) throws Exception {
+		DailyRecord record = null;
+	    try (Connection con = ds.getConnection()) {
+	      String sql = "SELECT * FROM daily_records WHERE user_id = ? "
+	      		+ " ORDER BY registered DESC, id DESC "
+	      		+ " LIMIT 1";
+	      PreparedStatement stmt = con.prepareStatement(sql);
+	      stmt.setInt(1, userId);
+	      ResultSet rs = stmt.executeQuery();
+	      if (rs.next()) {
+	        record = mapToDailyRecord(rs);
+	      }
+	    } catch (Exception e) {
+	      throw e;
+	    }
+	    return record;
+	}
+
+	@Override
+	public void insert(DailyRecord record) throws Exception {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+
+	@Override
+	public void update(DailyRecord record) throws Exception {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+
+	@Override
+	public void delete(int id, int userId) throws Exception {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+
+	protected DailyRecord mapToDailyRecord(ResultSet rs) throws Exception {
+		DailyRecord record = new DailyRecord();
+		record.setId(rs.getInt("id"));
+		record.setUserId(rs.getInt("user_id"));
+		record.setRegistered(rs.getTimestamp("registered"));
+		record.setWeight(rs.getDouble("weight"));
+		record.setWalk(rs.getInt("walk"));
+		record.setJogging(rs.getInt("jogging"));
+		record.setPool(rs.getInt("pool"));
+		record.setTraining(rs.getInt("training"));
+		record.setBall(rs.getInt("ball"));
+		record.setOther(rs.getInt("other"));
+		return record;
+	}
+	
+}
